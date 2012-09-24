350e0300cc84ad89325337ef7229c57e5bda0aef
csut017 <craig.j.sutherland@gmail.com>
2012-09-24 12:43:50 +1200

    Added an initial project model

---
+++ project/CruiseControlNet.SelfHost.Console/Models/ProjectSummary.cs
@@ -0,0 +1,17 @@
+﻿namespace CruiseControlNet.SelfHost.Console.Models
+{
+    /// <summary>
+    /// The model for a project summary.
+    /// </summary>
+    public class ProjectSummary
+    {
+        #region Public properties
+        #region Name
+        /// <summary>
+        /// Gets or sets the project name.
+        /// </summary>
+        public string Name { get; set; }
+        #endregion
+        #endregion
+    }
+}
